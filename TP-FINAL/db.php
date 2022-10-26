f8dbcd9129545fa2337c4ea5c9beed0ba26de6ae
Jpasqu <115999936+Jpasqu@users.noreply.github.com>
2022-10-26 12:23:30 +0200

    Update db.php

--- TP-FINAL/db.php
+++ TP-FINAL/db.php
@@ -1,5 +1,11 @@
 <?php
-
    // ICI DOIT APPARAITRE LA CONNECTION AVEC LA BASE DE DONNEES
    // ET LA GESTION DES ERREURS
+
+$server ="localhost";
+$user = "root";
+$pass = "root";
+$database = "users";
+
+$conn = mysqli_connect(^server, $user, $pass, $database);
 ?>
